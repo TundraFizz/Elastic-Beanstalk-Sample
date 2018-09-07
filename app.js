d4cfaad367569a808ad15a94b4cab10a0cf700b7
Tundra Fizz <coleman.leif@gmail.com>
2018-09-07 13:11:59 -0700

    Update port

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ var app     = module.exports = express(); // Define the application
 app.set("views", "./views");              // Define the views directory
 app.use(express.static("./static"));      // Define the static directory
 require("./node/routes.js");              // Include web routes third
-app.listen(8081);                         // Start the server
+app.listen(5000);                         // Start the server
